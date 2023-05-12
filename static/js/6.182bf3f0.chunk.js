b7ce7d553d77c7d4cdcd97a8d19180ec2bc502fc
Butcherok <110961800+Butcherok@users.noreply.github.com>
2023-05-12 12:59:52 +0000

    Deploying to gh-pages from @ Butcherok/goit-react-hw-08-phonebook@4f69cb45bb407edb4190ae53e72bd4e68146b1b3 🚀

---
+++ static/js/6.182bf3f0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[6],{3006:function(e,r,n){n.r(r);var t=n(184);r.default=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("h1",{children:(0,t.jsx)("span",{children:"Hello! i am your PhoneBook."})})})}}}]);
+//# sourceMappingURL=6.182bf3f0.chunk.js.map
